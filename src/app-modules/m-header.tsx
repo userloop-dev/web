a8686466d18f49d4cb0f385f65f9a7b6c8029fdc
Matt Hobbs <matt@userloop.dev>
2024-03-10 01:12:42 -0800

    Remove See Demo Button

--- src/app-modules/m-header.tsx
+++ src/app-modules/m-header.tsx
@@ -11,7 +11,7 @@ interface M_Header_Props extends HTMLAttributes<HTMLDivElement> {}
 
 export const M_Header = ({}: M_Header_Props) => (
 	<div className='relative w-full flex flex-col items-center py-24 pt-16'>
-		<div className='z-30 text-7xl md:text-9xl text-grayscale-600 font-semibold tracking-tighter max-w-xl text-center opacity-50'>{headerDim}</div>
+		<div className='z-30 text-7xl md:text-9xl text-grayscale-500 font-semibold tracking-tighter max-w-xl text-center'>{headerDim}</div>
 		<div className='z-30 text-7xl md:text-9xl text-grayscale-600 font-semibold tracking-tighter max-w-xl text-center mb-6'>{headerLit}</div>
 		<div className='z-30 text-sm text-grayscale-500 font-normal max-w-xl text-center leading-relaxed mb-6 px-6'>{headerDescription}</div>
 
@@ -20,10 +20,10 @@ export const M_Header = ({}: M_Header_Props) => (
 				Create an account
 				<ArrowUpRight className='h-4 w-4' />
 			</Button>
-			<Button variant='secondary' className='text-sm'>
+			{/* <Button variant='secondary' className='text-sm'>
 				Watch a product demo
 				<Play className='h-4 w-4' />
-			</Button>
+			</Button> */}
 		</div>
 
 		<div className='z-10 absolute pointer-events-none inset-0 h-full w-full bg-transparent bg-[linear-gradient(to_right,#ffffff10_1px,transparent_1px),linear-gradient(to_bottom,#ffffff10_1px,transparent_1px)] bg-[size:50px_50px]'></div>
