bf9c318c019a4b5719f754fb312697dfceabcdb5
Matt Hobbs <matt@userloop.dev>
2024-03-28 20:12:18 -0700

    Add GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '../globals.scss';
 import clsx from 'clsx';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,21 +15,13 @@ export default (ctx: any) => (
 				content="Uncover the insights lurking within your application data. Intergrate with a simple Events API to effortlessly capture every event with precision. It's not just about collecting data; it's about discovering the stories they tell."
 			/>
 			<link rel='shortcut icon' href='/favicon.png' />
-
-			<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16492043817">
-			</script>
-			<script>
-				window.dataLayer = window.dataLayer || [];
-				function gtag(){dataLayer.push(arguments);}
-				gtag('js', new Date());
-
-				gtag('config', 'AW-16492043817');
-			</script>
 		</head>
 
 		<body className='bg-grayscale-100'>
 			{ctx.children}
 			<Analytics />
 		</body>
+
+		<GoogleAnalytics gaId='AW-16492043817' />
 	</html>
 );
