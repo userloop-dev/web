86345cfaa5aad4305ff80aebf39bd5983301918c
Matt Hobbs <matt@userloop.dev>
2024-03-28 20:08:10 -0700

    Add Google Tracking Tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,16 @@ export default (ctx: any) => (
 				content="Uncover the insights lurking within your application data. Intergrate with a simple Events API to effortlessly capture every event with precision. It's not just about collecting data; it's about discovering the stories they tell."
 			/>
 			<link rel='shortcut icon' href='/favicon.png' />
+
+			<script async src="https://www.googletagmanager.com/gtag/js?id=AW-16492043817">
+			</script>
+			<script>
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+
+				gtag('config', 'AW-16492043817');
+			</script>
 		</head>
 
 		<body className='bg-grayscale-100'>
